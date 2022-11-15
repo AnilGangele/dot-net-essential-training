89b3a527ac59b367984fc1dff4c4c0d5151edb95
Anil Gangele <anil.gangele@gmail.com>
2022-11-15 09:19:56 +0530

    commiting changes from local

--- Start/Overview/MyConsoleApp/Program.cs
+++ Start/Overview/MyConsoleApp/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+String response;
+Console.WriteLine("Whats your name buddy?");
+response = Console.ReadLine();
+
+Console.WriteLine($"Enojy learning Mr {response}");
+
+
